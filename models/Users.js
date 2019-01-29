dad3c680b5027f86b2a7719cb935065face652dc
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-01-29 14:58:45 -0500

    User moda

--- models/Users.js
+++ models/Users.js
@@ -5,3 +5,23 @@ const Schema = mongoose.Schema
 
 module.exports = mongoose.model('Idea', Idea)
 
+const mongoose = require("../db/connections");
+const Schema = mongoose.Schema;
+
+const User = new Schema ({
+     name: String,
+     category: String,
+     image: String,
+     username: String,
+     password: String,
+     entertain: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Category'
+        }
+    ]
+});
+ 
+
+
+module.exports = mongoose.model("User", User);
