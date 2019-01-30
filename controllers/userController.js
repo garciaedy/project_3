4eef3b7bedddd327790e0e85e01a67bb3e76bb26
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-01-30 11:13:33 -0500

    index created

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,19 @@ const User = require('../models/User')
 const Category = require("../models/Category")
 
 const userController = {
+index: (req, res) =>{
+    User.find({}).then((users) =>{
+res.send(users)
+    })
+},
+
+
+
+
+
+
+
+
 
 }
 
