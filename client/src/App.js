e33107ce1f92abb31f50c42d3dfc0de151c43e1e
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-01-30 17:18:37 -0500

    initialized css

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './components/NavBar'
+import UserPage from './components/UserPage'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import HomePage from './components/HomePage'
+import CategoryPage from './components/CategoryPage'
+import LogInPage from './components/LogInPage'
+import { createGlobalStyle } from 'styled-components'
+
 import './App.css';
+import CategoryPage from './components/CategoryPage';
+
+const Global = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Amatic+SC');
+  
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Amatic SC', cursive;
+    background: white;
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div>
+        <Global />
+        <NavBar />
+        <Switch>
+          <Route exact path="/login" component={LogInPage}/>
+          <Route exact path="/user/:userId" component={CategoryPage}/>
+          <Route exact path ="/user" component={UserPage}/>
+          <Route path="/" component={HomePage}/>
+
+        </Switch>
+       
       </div>
+    </Router>
     );
   }
 }
