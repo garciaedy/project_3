0d9d6250579734968e781135d86b738da8cbd167
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-01-30 17:19:16 -0500

    update

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 
+const NavBarStyles = styled.div`
+display: flex;
+justify-content: space-between;
+background: #2d9b90;
+height: 60px;
 
-
+a{
+    text-decoration: none;
+    padding-left: 10px;
+    color: white;
+    &:active {
+        color: red;
+      }
+}
+.right {
+    width: 15vw;
+    display: flex;
+    justify-content: space-around;
+  }
+`;
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        
+      <NavBarStyles id="nav-container" class="some-class"> 
+      <Link to="/" >Main</Link>
+      <div className="right">
+<Link to="/login" > Sing Up</Link>
+<Link to="/user" > Forum </Link>
       </div>
+      </NavBarStyles>
     )
   }
 }
