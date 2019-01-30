270cb437ba583cabe2723d9699828b02cf1a5f9a
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-01-30 13:09:42 -0500

    components initiliazed

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ client/src/components/SingUpForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingUpForm extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
