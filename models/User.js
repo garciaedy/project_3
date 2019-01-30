c29e554ccd47c7e41a18a81207fc953a7abcb3b1
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-01-30 10:39:33 -0500

    updated

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('../db/connections')
+const Schema = mongoose.Schema
+
+const User = new Schema ({
+     
+     image: String,
+     username: String,
+     password: String,
+     entertain: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Category'
+        }
+    ]
+});
+ 
+
+
+module.exports = mongoose.model("User", User);
