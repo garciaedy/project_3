b52ec97e8ff2d1b511802bc5f6145965201aca4a
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-02-01 20:47:44 -0500

    state

--- client/src/components/newCatForm.js
+++ client/src/components/newCatForm.js
@@ -1,8 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import {Button} from 'react-materialize'
+import styled from 'styled-components'
 
-
+const FormStyle = styled.div`
+color: black;
+`;
 
 export default class newCatForm extends Component {
+state ={
+newCat:{
+  name: "",
+  genre: "",
+  year: "",
+  category: "",
+  image: ""
+}
+};
+handleChnage = e =>{
+  console.log("name",e.target.name);
+  console.log("value", e.target.value);
+  const createdEntertain ={...this.state.newCat};
+  this.setState({newCat: createdEntertain})
+}
+
+handleSubmit = event =>{
+  event.preventDefault();
+  const payload={
+    name: this.state.newCat.name,
+    genre: this.state.newCat.genre,
+    year: this.state.newCat.year,
+    category: this.state.newCat.category,
+    image: this.state.newCat.image
+
+  };
+  const userId = this.props.match.params.userId;
+  axios.post(`/api/users/${userId}/categories`, payload).then(res =>{
+    console.log(res.data);
+    this.props.history.go(`/users/${userId}`);
+  })
+}
+
+
   render() {
     return (
       <div>
