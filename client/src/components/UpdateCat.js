0a22b5dbdf75dedae4ed28c9e33c002c7d79f0ce
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-02-03 12:57:00 -0500

    component

--- client/src/components/UpdateCat.js
+++ client/src/components/UpdateCat.js
@@ -8,11 +8,29 @@ export default class UpdateCat extends Component {
     entertainment: []
   }
   componentDidMount() {
-    const categoriesId = this.props.match.params.CatId;
-    axios.get(`/api/users${userId}`).then(res =>{
-      this.setState({ user: res.data});
+    const categoriesId = this.props.match.params.categoriesId;
+    const userId = this.props.match.params.userId;
+    axios.get(`/api/users${userId}/categories/${categoriesId}`).then(res =>{
+      console.log(res.data);
+      this.setState({ entertainment: res.data});
     })
   }
+    
+handleChange = e => {
+  console.log("name",e.target.name)
+  console.log("value", e.target.value);
+  const updatedEntertain = {...this.state.entertainment}
+  this.setState({entertainment: updatedEntertain})
+}
+  handleUpdate = categoriesId => {
+    const updatedEntertain = this.state.entertainment;
+    const userId = this.props.match.params.userId;
+    axios.patch(`/api/user/${userId}/categories/${categoriesId}`, updatedEntertain)
+    .then(()=>{
+      console.log("updated Cat")
+    })
+  }
+  
   const 
   render() {
     return (
