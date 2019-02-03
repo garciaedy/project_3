b7632aca0c094e948a098e983e8ff5eef8d76b7c
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-02-03 11:36:45 -0500

    component

--- client/src/components/UpdateCat.js
+++ client/src/components/UpdateCat.js
@@ -4,6 +4,16 @@ import {Button} from 'react-materialize'
 
 
 export default class UpdateCat extends Component {
+  state ={
+    entertainment: []
+  }
+  componentDidMount() {
+    const categoriesId = this.props.match.params.CatId;
+    axios.get(`/api/users${userId}`).then(res =>{
+      this.setState({ user: res.data});
+    })
+  }
+  const 
   render() {
     return (
       <div>
