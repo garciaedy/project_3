983860b1849b0cfe33002e75fa704bb051621b0f
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-02-03 21:42:47 -0500

    routes finally working

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,10 @@ router.patch('/api/users/:userId', userController.update)
 
 
 router.get('/api/users/:userId/categories', categoryController.index)
-router.get('/api/users/:userId/categories/:categoriesId', categoryController.show)
 router.post('/api/users/:userId/categories', categoryController.create)
-router.patch('/api/users/:userId/categories/:categoriesId', categoryController.update)
-router.delete('/api/users/:userId/categories/:categoriesId',categoryController.delete)
+router.get('/api/categories/:categoriesId', categoryController.show)
+router.patch('/api/categories/:categoriesId', categoryController.update)
+router.delete('/api/categories/:categoriesId',categoryController.delete)
 // // router.get('/api/users/:userId/ideas', ideasController.index)
 // // router.get('/api/ideas/:ideaId', ideasController.show)
 // // router.delete('/api/ideas/:ideaId', ideasController.delete)
