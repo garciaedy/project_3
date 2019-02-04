a6ccec35b6868719940c63f197f5a7eed5e54dcc
Edy Garcia <edygarcia@Edys-MacBook-Pro.local>
2019-02-04 08:09:05 -0500

    routes

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,6 @@ router.post('/api/users/', userController.create)
 router.get('/api/users/:userId', userController.show)
 router.patch('/api/users/:userId', userController.update)
 router.delete('/api/users/:userId', userController.delete)
-// router.get('/api/users', userController.index)
-// router.get('/api/users/:userId', userController.show)
-// router.post('/api/users', userController.create)
-// router.delete('/api/users/:userId', userController.delete)
-// router.patch('/api/users/:userId', userController.update)
 
 
 router.get('/api/users/:userId/categories', categoryController.index)
@@ -21,11 +16,6 @@ router.post('/api/users/:userId/categories', categoryController.create)
 router.get('/api/categories/:categoryId', categoryController.show)
 router.patch('/api/categories/:categoryId', categoryController.update)
 router.delete('/api/categories/:categoryId',categoryController.delete)
-// // router.get('/api/users/:userId/ideas', ideasController.index)
-// // router.get('/api/ideas/:ideaId', ideasController.show)
-// // router.delete('/api/ideas/:ideaId', ideasController.delete)
-// // router.patch('/api/ideas/:ideaId', ideasController.update)
-// // router.post('/api/users/:userId/ideas', ideasController.create)
 
 module.exports = router
 
