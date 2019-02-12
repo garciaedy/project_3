f4e5a24d771c24c31c1ba774e3ccdeced2af018c
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-02-11 23:39:45 -0500

    heroku

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/userController')
-const categoryController = require('../controllers/categoryController')
+const userController = require('../controllers/user')
+const videoGamesController = require("../controllers/videoGame");
 
 router.get('/api/users', userController.index)
 router.post('/api/users/', userController.create)
