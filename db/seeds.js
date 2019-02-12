24d5af243ef6f7647fc1ef6a069f3d533fdead1d
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-02-11 22:53:43 -0500

    seeds

--- db/seeds.js
+++ db/seeds.js
@@ -3,72 +3,73 @@ const User = require("../models/User");
 const Category = require("../models/Category");
 
 
-const kombat = new Category({
+const kombat = new VideoGAme({
     name: "Mortal Kombat 11",
-    description: "fighting",
-    // year: 2019,
-    // category: "game",
-    image: "https://mortalkombat.com/preorder?gclid=Cj0KCQiAkMDiBRDNARIsACKP1FEv3kovPJTL5q19Gmch_opatqamQk0iB1QBIa6c7R61e24WIIyHkBAaAsmNEALw_wcB"
+    year: 2019,
+    category: "game",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/Mortal_Kombat_11_cover_art.png/220px-Mortal_Kombat_11_cover_art.png",
+    description: "fighting"
 
 })
-const dBall = new Category({
+const dBall = new VideoGame({
     name: "dragon ball super: broly",
-   description: "amime",
-    // year: 2018,
-    // category: "movie",
-    image: "https://www.youtube.com/watch?v=FHgm89hKpXU"
-
+    
+    category: "movie",
+    image: "https://media.comicbook.com/2017/09/dragon-ball-broly-strongest-fighter-universe-7-1022229-1280x0.jpg",
+    year: 2018,
+description: "fighting",
 })
-const daWeb = new Category({
+const daWeb = new VideoGame({
     name: "spider-man: into the spider-verse",
+    image: "http://intothespiderverse.movie/images/share/shareImage.jpg",
+    category: "movie",
+     year: 2018,
     description: "anime",
-    // year: 2018,
-    // category: "movie",
-    image: "https://www.youtube.com/watch?v=FU0i0eg6qak"
 
 })
-const sun = new Category({
+const sun = new VideoGame({
     name: "The Sun Dog",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71LIUwoPc3L.jpg",
+    category: "book",
+    year: 2017,
     description: "Horror",
-    // year: 2017,
-    // category: "book",
-    image: "https://www.barnesandnoble.com/w/the-sun-dog-stephen-king/1013077297?ean=9781982115425#/"
+
 
 })
-const vol1 = new Category({
+const vol1 = new VideoGame({
     name: "For the Kid I Saw in My Dreams, Vol. 1",
+    image: "https://cdn.animeuknews.net/app/uploads/2019/01/For-the-Kid-I-Saw-In-my-dreams-volume-1-cover-170x244.jpg",
+    category: "book",
+    year: 2018,
     description: "fantacy",
-    // year: 2018,
-    // category: "book",
-    image: "https://www.barnesandnoble.com/w/for-the-kid-i-saw-in-my-dreams-vol-1-kei-sanbe/1129915926?ean=9781975328863#/"
 
 })
-const smash = new Category({
+const smash = new VideoGame({
     name: "Super Smash Bros. Ultimate",
+    image:"https://i.imgur.com/4aAixLM.png"
+      category: "game",
+     year: 2018,
     description: "fighting",
-    // year: 2018,
-    // category: "game",
-    image: "https://www.smashbros.com/en_US/"
 
 })
 const edy = new User ({
     username: 'edygarcia',
     password: 'project3' ,
-    image: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjJooaE7JPgAhVGNd8KHSNZADcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D2ryxpNewOHg&psig=AOvVaw0YCygJEz8pDnIWJ1TYScfl&ust=1548880603992617",
-    categories:[dBall,sun, smash]
+    image: "https://images.unsplash.com/photo-1542838687-f960f044ef1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+    games:[dBall,sun, smash]
 })
 const aaron = new User ({
     username: 'garciaedy',
-    password: '3project',
-    image: "https://www.google.com/search?q=react.js+express&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjy5J6B7JPgAhUtmeAKHR36AZsQ_AUIECgD&biw=1195&bih=722#imgrc=AzscrIxgOWCrlM:",
-  categories:[ kombat, daWeb, vol1]
+   console: '3project',
+    image: "https://images.unsplash.com/photo-1549482287-9237fdb5bec0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+  games:[ kombat, daWeb, vol1]
 
 });
 
 
 
 User.remove({})
-    .then(() => Category.remove({}))
+    .then(() => VideGame.remove({}))
     .then(() => Category.insertMany([kombat, dBall, daWeb, sun, vol1, smash]))
     .then(() => edy.save())
     .then(() => aaron.save())
